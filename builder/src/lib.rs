9a25e618d985f4e2c46532acd7a152e10a508262
Alex Vzorov <alex.vzorov@gmail.com>
2023-01-17 14:27:39 +0800

    feat: reformat generated source in one go

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,7 @@
 use proc_macro::TokenStream;
-use quote::{format_ident, quote, ToTokens};
+use quote::{format_ident, quote};
 use std::fs;
+use std::process::Command;
 use syn::{parse_macro_input, Data::*, DeriveInput, Fields::*};
 
 #[proc_macro_derive(Builder)]
@@ -85,6 +86,15 @@ pub fn derive(input: TokenStream) -> TokenStream {
     };
 
     let ts = proc_macro::TokenStream::from(expanded);
-    fs::write("gen.rsg", ts.to_string()).unwrap();
+    save_and_format(&ts, "builder.rs");
     ts
+}
+
+/// saves the token stream into a file, and tries to reformat it
+fn save_and_format(ts: &TokenStream, path: &str) {
+    fs::write(path, ts.to_string()).unwrap();
+    Command::new("rustfmt")
+        .arg(path)
+        .output()
+        .expect("failed to format the generated code");
 }
